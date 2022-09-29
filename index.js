1291cbf5f21aa60a35c7d8edaf160d31bb96b95d
Parikshitdeore <113280255+Parikshitdeore@users.noreply.github.com>
2022-09-29 14:22:10 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var inputdate=document.querySelector("#input-date")
+var button=document.querySelector("#button")
+var output = document.querySelector("output")
+
+button.addEventListener("click",checkIfPalindrome)
+
+function checkIfPalindrome(){
+console.log(typeof(inputdate.value))
+}
