7ead87d0e04f16b5b25120bb593da53d1478a5c1
Parikshitdeore <113280255+Parikshitdeore@users.noreply.github.com>
2022-09-29 18:50:48 +0530

    part 1 completed

--- index.js
+++ index.js
@@ -5,5 +5,55 @@ var output = document.querySelector("output")
 button.addEventListener("click",checkIfPalindrome)
 
 function checkIfPalindrome(){
-console.log(typeof(inputdate.value))
+var date = getDate(inputdate.value)
+var palindrome=isPalindromeForAllFormats(date)
+}
+
+function reverseStr(date){
+var chars=date
+chars = chars.split('');
+var revchars = chars.reverse();
+var reversed=revchars.join('')
+return reversed
+
+}
+
+function isPalindrome(date){
+    var str=date[0];
+    var reversed=reverseStr(str)
+    return str===reversed;
+}
+
+function getDate(userdate){
+    var splitdate = userdate.split("-")
+    var date={
+        day:splitdate[2],
+        month:splitdate[1],
+        year:splitdate[0]
+    }
+return date
+}
+
+function getAllDateFormats(date){
+    var ddmmyyyy=[date.day+date.month+date.year]
+    var mmddyyyy=[date.month+date.day+date.year]
+    var yyyymmdd=[date.year+date.month+date.day]
+    var ddmmyy=[date.day+date.month+date.year.slice(-2)]
+    var mmddyy=[date.month+date.day+date.year.slice(-2)]
+    var yymmdd=[date.year.slice(-2)+date.month+date.day]
+    return [ddmmyyyy,mmddyyyy,yyyymmdd,ddmmyy,mmddyy,yymmdd]
+}
+
+function isPalindromeForAllFormats(date){
+var listOfPalindrome=getAllDateFormats(date)
+var palindrome=false
+for (let i = 0; i < listOfPalindrome.length; i++){
+    
+    if(isPalindrome(listOfPalindrome[i])===true){
+        palindrome=true;
+        break;
+    }
+}
+console.log(palindrome)
+return palindrome
 }
